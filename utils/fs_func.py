754744507e21dc49d0b400adab0246fb873c160a
Goleg <goleg130267@gmail.com>
2024-08-06 07:29:33 +0300

    instant commit

--- utils/fs_func.py
+++ utils/fs_func.py
@@ -1,91 +1,122 @@
 import os
 import shutil
 import sys
+from functools import wraps
 
-def print_items(msg,items):
+def log_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Вызов функции {func.__name__}")
+        result = func(*args, **kwargs)
+        print(f"Функция {func.__name__} завершена")
+        return result
+    return wrapper
+
+def exception_handler(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            print('Ошибка: Ввод должен быть числом')
+        except IndexError:
+            print('Ошибка: Нет такого пункта')
+        except FileNotFoundError:
+            print('Ошибка: Файл или директория не существуют.')
+        except PermissionError:
+            print('Ошибка: Недостаточно прав для выполнения операции.')
+        except OSError as e:
+            print(f'Ошибка: {e}')
+        except Exception as e:
+            print(f'Непредвиденная ошибка: {e}')
+    return wrapper
+
+@log_decorator
+@exception_handler
+def print_items(msg, items):
     print(msg)
     for item in items:
-         print(f"{items.index(item)} - {item}")
+        print(f"{items.index(item)} - {item}")
 
+@log_decorator
+@exception_handler
 def list_dir():
-    items=[item for item in os.listdir()]
+    items = [item for item in os.listdir()]
     return items
 
+@log_decorator
+@exception_handler
 def is_file(item):
-    items=list_dir()
+    items = list_dir()
     current_directory = os.getcwd()    
     if os.path.isfile(os.path.join(current_directory, items[item])):
         return True
     elif os.path.isdir(os.path.join(current_directory, items[item])):
         return False
-    
+
+@log_decorator
+@exception_handler
 def list_files():
     current_directory = os.getcwd()
     files = [item for item in list_dir() if os.path.isfile(os.path.join(current_directory, item))]
     return files
 
+@log_decorator
+@exception_handler
 def list_folders():
     current_directory = os.getcwd()
     folders = [item for item in list_dir() if os.path.isdir(os.path.join(current_directory, item))]
     return folders
 
+@log_decorator
+@exception_handler
 def ask_todo(input_msg):
-    items=list_dir()
-    print_items("Содержимое текущей папки:",items)
-    print ('='*20)
-    item=input(input_msg)
-    if not item.isdigit():
-        print('Ошибка!')
-        return False
-    else:
-        item=int(item)
-    if not (0 <= item < len(items)):
-        print('Ошибка!')
-        return False
-    else:
-        return item
+    items = list_dir()
+    print_items("Содержимое текущей папки:", items)
+    print('=' * 10)
+    item = int(input(input_msg))
+    selected_item = items[item]
+    return item
 
-def new_dir():
-    new_dir=input('Введите наименование папки:')
-    if not os.path.exists(new_dir):
-        os.mkdir(new_dir)
-    else:
-        print('Ошибка. Такая папка уже существует.')
+@log_decorator
+@exception_handler
+def new_dir():    
+    new_dir = input('Введите наименование папки:')
+    os.mkdir(new_dir)
+    return True
 
+@log_decorator
+@exception_handler
 def rm_dir():
-    items=list_dir()
-    item=ask_todo('Введите номер элемента для удаления: ')
-    if item ==False:
-        return
-    if is_file(item) :
+    items = list_dir()
+    item = ask_todo('Введите номер элемента для удаления: ')
+    if is_file(item):
         os.remove(items[item])
     else:
         os.rmdir(items[item])
-    items=list_dir()
-    print_items("Содержимое текущей папки:",items)
+    print(f'Элемент {items[item]} успешно удален')
+    items = list_dir()
+    print_items("Содержимое текущей папки:", items)
+    return True
 
+@log_decorator
+@exception_handler
 def copy_item():
-    items=list_dir()
-    item=ask_todo('Введите номер элемента для копирования: ')
-    if item == False:
-        return
-    new_name=input(f"Введите имя элемента для копирования {items[item]}: ")
+    items = list_dir()
+    item = ask_todo('Введите номер элемента для копирования: ')
+    new_name = input(f"Введите имя элемента для копирования {items[item]}: ")
     if is_file(item):
         shutil.copy(items[item], new_name)
     else:
         shutil.copytree(items[item], new_name)
     print_items("Содержимое текущей папки:", list_dir())
+    return True
 
+@log_decorator
+@exception_handler
 def come_to_dir():
-
-    # Вывод текущего рабочего каталога
     print("Текущий рабочий каталог:", os.getcwd())
-
-    # Путь к новому рабочему каталогу
-    new_directory=input('Введите путь для смены текущего каталога: ')
-
-    # Смена рабочего каталога
+    new_directory = input('Введите путь для смены текущего каталога: ')
     os.chdir(new_directory)
-
-    # Проверка, что рабочий каталог изменен
     print("Новый рабочий каталог:", os.getcwd())
+    return True
